d9c9eee2e5572ab85c8cb33e608ccbc482784e6a
Iliyan-Y <maximizebg@gmail.com>
2022-09-25 15:44:24 +0100

    create new task fn

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+use std::io;
+
 use ncurses::*;
 
-fn main() {
+fn init_ncurses() {
     initscr();
     noecho();
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+    clear();
+}
 
+fn main() {
+    init_ncurses();
     let mut quit = false;
-    let mut todo_list = Vec::from(["Read book", "Play with the kids", "Walk the wife"]);
+    let mut todo_list = Vec::from([
+        "Read book".to_string(),
+        "Play with the kids".to_string(),
+        "Walk the wife".to_string(),
+    ]);
 
     display_todo_list(&mut todo_list);
 
@@ -15,15 +25,14 @@ fn main() {
         match key as u8 as char {
             'q' => quit = true,
             // ARROW KEYS
-            'A' => quit = true,                 // up
-            'B' => quit = true,                 // down
-            'D' => add_to_list(&mut todo_list), // left
-            'C' => quit = true,                 // right
-            ' ' => quit = true,                 // SPACE
-            '\n' => quit = true,                // ENTER
-            // escae '`' => quit = true,
+            'A' => quit = true, // up
+            'B' => quit = true, // down
+            'D' => quit = true, // left
+            'C' => quit = true, // right
+            // add new todo
+            ' ' => quit = true,                      // SPACE
+            '\n' => create_new_task(&mut todo_list), // ENTER
             _ => {
-                // TODO: use to add new todo ???
                 let key_as_char = key as u8 as char;
                 addstr(&key_as_char.to_string());
             }
@@ -33,7 +42,7 @@ fn main() {
     endwin();
 }
 
-fn display_todo_list(todo_list: &mut Vec<&str>) {
+fn display_todo_list(todo_list: &mut Vec<String>) {
     for (index, todo) in todo_list.iter().enumerate() {
         mv(index as i32, 1);
         addstr(todo);
@@ -44,8 +53,15 @@ fn display_todo_list(todo_list: &mut Vec<&str>) {
     refresh();
 }
 
-fn add_to_list(todo_list: &mut Vec<&str>) {
-    clear();
-    todo_list.push("new value");
+fn create_new_task(todo_list: &mut Vec<String>) {
+    let mut new_task = String::new();
+
+    endwin();
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    println!("Enter task name: ");
+    io::stdin().read_line(&mut new_task).expect("error ");
+
+    todo_list.push(new_task);
+    init_ncurses();
     display_todo_list(todo_list);
 }
