8754224fb2c20f6f077db7601e755cdaff394b6e
Iliyan-Y <maximizebg@gmail.com>
2022-09-25 16:04:44 +0100

    code refactored

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,18 @@ fn main() {
 
     while !quit {
         let key = getch();
+
         match key as u8 as char {
             'q' => quit = true,
             // ARROW KEYS
-            'A' => quit = true, // up
-            'B' => quit = true, // down
-            'D' => quit = true, // left
-            'C' => quit = true, // right
-            // add new todo
+            'A' => quit = true,                      // up
+            'B' => quit = true,                      // down
+            'D' => quit = true,                      // left
+            'C' => quit = true,                      // right
             ' ' => quit = true,                      // SPACE
             '\n' => create_new_task(&mut todo_list), // ENTER
             _ => {
-                let key_as_char = key as u8 as char;
+                let key_as_char = key as u8;
                 addstr(&key_as_char.to_string());
             }
         }
@@ -49,19 +49,27 @@ fn display_todo_list(todo_list: &mut Vec<String>) {
     }
 
     mv(todo_list.len() as i32 + 1, 1);
-    addstr("Press q to quit");
+    addstr("-------------------MENU----------------------");
+    addstr("\n Press ENTER to add new task\n Press q to quit");
     refresh();
 }
 
 fn create_new_task(todo_list: &mut Vec<String>) {
-    let mut new_task = String::new();
-
     endwin();
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+
+    let mut new_task = String::new();
+    clean_up_terminal();
     println!("Enter task name: ");
     io::stdin().read_line(&mut new_task).expect("error ");
 
-    todo_list.push(new_task);
+    if !(new_task.trim().is_empty()) {
+        todo_list.push(new_task);
+    }
+
     init_ncurses();
     display_todo_list(todo_list);
+}
+
+fn clean_up_terminal() {
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 }
