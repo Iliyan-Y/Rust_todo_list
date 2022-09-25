1ee827907c1027d86b86d6bcfe70943890e8d9ef
Iliyan-Y <maximizebg@gmail.com>
2022-09-25 11:00:02 +0100

    todo list created + display

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
 
     let mut quit = false;
+    display_todo_list();
 
-    addstr("Hello, world!");
     refresh();
     while !quit {
         let key = getch();
@@ -18,4 +18,16 @@ fn main() {
     }
 
     endwin();
+}
+
+fn display_todo_list() {
+    let mut todo_list = Vec::from(["Read book", "Play with the kids", "Walk the wife"]);
+
+    for (index, todo) in todo_list.iter().enumerate() {
+        mv(index as i32, 1);
+        addstr(todo);
+    }
+
+    mv(todo_list.len() as i32 + 1, 1);
+    addstr("Press q to quit");
 }
